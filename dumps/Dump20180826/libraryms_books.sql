18c4e52c7ffc67319c4bd413652d1eb2fdd52be3
Hindhu <jerronva@gmail.com>
2018-08-26 16:28:52 +0530

    dumps

---
+++ dumps/Dump20180826/libraryms_books.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `libraryms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `libraryms`;
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: libraryms
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `books` (
+  `BookId` varchar(50) NOT NULL,
+  `Title` varchar(45) DEFAULT NULL,
+  `Author` varchar(45) DEFAULT NULL,
+  `MainClassification` varchar(45) DEFAULT NULL,
+  `SubClassification` varchar(45) DEFAULT NULL,
+  `PublishedYear` varchar(4) DEFAULT NULL,
+  `LastPrinted` varchar(4) DEFAULT NULL,
+  `ISBNNumber` varchar(45) DEFAULT NULL,
+  `NumberOfPages` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`BookId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES ('002','Wings of Fire','A.P.J.Abdul Kalam','Documentation','Biography','1999','2016','456','400'),('003','Wings of Fire','A.P.J.Abdul Kalam','Documentation','Biography','1999','2016','456','400'),('004','Wings of Fire','A.P.J.Abdul Kalam','Documentation','Biography','1999','2016','456','400'),('005','The Monk who sold his ferrari','Robin Sharma','Arts','Motivation','2010','2017','123','200'),('0075','Olivwer Twist','Charles Dickens','Novel','Story','1834','2018','464565','559'),('010','Jane Eyre','Charlotte Bronte','Novel','Story','1847','2016','s5656','587'),('012','','','','','','','',''),('13','3fdwf','wf','wef','ef','ef','ef','2','12');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-26 16:25:55
